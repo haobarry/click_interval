be908806e414bb4fdf500370e6f90ccdadeaf263
zb <binbuy@126.com>
2020-08-19 12:04:21 +0800

    Create interval.js

---
+++ interval.js
@@ -0,0 +1,10 @@
+  
+//倒数五个数 点击
+var loops = function(){ for (let i=5, j=1; i<0,j<6; i--,j++) {
+    (function (i) {
+      setTimeout(() => console.log(i), 1000*j)
+    })(i)
+  }
+};
+var bbb = document.getElementById('ext-gen470');
+var timer = window.setInterval(function(){bbb.click(loops())},6000);
