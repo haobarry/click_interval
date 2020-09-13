3c4ac0bf6578f32738e399b9337b028d2dd81199
zb <binbuy@126.com>
2020-09-14 03:14:10 +0800

    Update interval.js
    不用强制执行

--- interval.js
+++ interval.js
@@ -1,9 +1,9 @@
   
 //倒数五个数 点击
 var loops = function(){ for (let i=5, j=1; i<0,j<6; i--,j++) {
-    (function (i) {
+ 
       setTimeout(() => console.log(i), 1000*j)
-    })
+    
   }
 };
 var bbb = document.getElementById('ext-gen470');
